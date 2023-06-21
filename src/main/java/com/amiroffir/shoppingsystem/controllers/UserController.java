f4d88a9c16688c1d074f31009b29e2257c5455b6
Amiroffir <amirosoffir@gmail.com>
2023-06-21 15:55:10 +0300

    user update details

--- src/main/java/com/amiroffir/shoppingsystem/controllers/UserController.java
+++ src/main/java/com/amiroffir/shoppingsystem/controllers/UserController.java
@@ -1,9 +1,36 @@
 package com.amiroffir.shoppingsystem.controllers;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import com.amiroffir.shoppingsystem.models.Product;
+import com.amiroffir.shoppingsystem.models.User;
+import com.amiroffir.shoppingsystem.services.UserService;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin
 @RestController
 public class UserController {
+    @Autowired
+    private UserService userService;
+
+    //@PostMapping("/users/login")
+
+   // @PostMapping("/users/register")
+
+    @PutMapping("/users/update")
+    public ResponseEntity<User> updateUser(@RequestBody User user) {
+        try {
+            User updatedUser = userService.updateUser(user);
+            return ResponseEntity.ok(updatedUser);
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        } catch (Exception e) {
+            // Log
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+
+    }
+
 }
