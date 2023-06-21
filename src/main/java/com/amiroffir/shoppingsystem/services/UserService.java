facca0595c48f993e11abce328eb1f55997c3484
Amiroffir <amirosoffir@gmail.com>
2023-06-21 15:55:46 +0300

    user update details

--- src/main/java/com/amiroffir/shoppingsystem/services/UserService.java
+++ src/main/java/com/amiroffir/shoppingsystem/services/UserService.java
@@ -1,7 +1,28 @@
 package com.amiroffir.shoppingsystem.services;
 
+import com.amiroffir.shoppingsystem.models.Product;
+import com.amiroffir.shoppingsystem.models.User;
+import com.amiroffir.shoppingsystem.repos.UserRepo;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    @Autowired
+    private UserRepo userRepo;
+
+    public User updateUser(User user) {
+        try {
+            if (!userRepo.existsById(user.getUserId())) {
+                throw new EntityNotFoundException();
+            }
+            return userRepo.save(user);
+        } catch (EntityNotFoundException e) {
+            throw e; // Rethrow the EntityNotFoundException
+        } catch (Exception e) {
+            // Log or handle other exceptions
+            throw e;
+        }
+    }
 }
